23cd80bbf4396805d5dfc8ff1c81f089d1f6fed4
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2020-03-31 01:31:39 -0300

    Página de login - ReactJS

---
+++ frontend-be-the-hero/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      {/* Permite que apenas uma rota seja executada por momento */}
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
