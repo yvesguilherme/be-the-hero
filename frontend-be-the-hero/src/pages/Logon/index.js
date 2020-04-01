c23d79196be0ae71e6f35e74d0a2a1176f4c7f59
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2020-04-01 01:11:44 -0300

    Adicionando os componentes NewIncident, Profile e alterações nas estilizações já existentes

--- frontend-be-the-hero/src/pages/Logon/index.js
+++ frontend-be-the-hero/src/pages/Logon/index.js
@@ -19,7 +19,7 @@ function Logon() {
           <input placeholder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
 
-          <Link to="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
           </Link>
--- frontend-be-the-hero/src/routes.js
+++ frontend-be-the-hero/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 function Routes() {
   return (
@@ -11,6 +13,9 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
